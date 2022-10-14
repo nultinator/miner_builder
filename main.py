3fd472105c9a18cd353d54c9db493051060b2872
Jake Nulty <nultinator@yahoo.com>
2022-10-13 21:43:16 -0400

    add: miniZ support
    add: handle bad input

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 ### possible deps ###
 
 
-
+### Get miner type
+print("Do you use miniZ or Gminer")
+miner = input()
+while miner.lower() != "gminer" and miner.lower() != "miniz":
+    print("Selection not valid, please choose either Gminer or miniZ")
+    miner = input()
 ### Get mining pool url
 print("please enter mining pool url\nexample: ycash.dapool.io:3344")
 pool_url = input()
@@ -11,7 +16,12 @@ print("please enter a ycash address\nexample: s1QZvkYSqZWGQwTpw3Zbe9VmScoinsDD5L
 ycash_address = input()
 
 ### Write information to a file
-miner_info = "./miner --algo 192_7 --pers ZcashPOW --server " + pool_url + " --user " + ycash_address
+if miner.lower() == "gminer":
+    miner_info = "./miner --algo 192_7 --pers ZcashPOW --server " + pool_url + " --user " + ycash_address
+elif miner.lower() == "miniz":
+    miner_info = "./miniZ --url=" + ycash_address + "@" + pool_url + " --par=192,7 --pers ZcashPOW -p x"
+else:
+    print("Something went wrong")
 miner_file =  open("mine_yec.sh", "w")
 miner_file.write(miner_info)
 miner_file.close()
